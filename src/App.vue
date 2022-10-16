66c06b57e145c9d8392e0aff7a85c0ee70f7eb36
Cristina <georgianacristinat@gmail.com>
2022-10-16 11:11:47 +0200

    finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-
     <div class="app">
-        <!-- <Tweet :tweetHijo = 'tweets' :otraInfoQuemMeLlegaDelPadre='2'/> -->
-       <Tweet :tweetHijo="tweets"/>
-       
-
+        <Tweet v-for="tweet in tweets" :tweet="tweet" :key="tweet.message" />
     </div>
 </template>
   
 <script setup>
-
 import Tweet from './components/Tweet.vue';
 
 const tweets = [
@@ -40,6 +35,7 @@ const tweets = [
         timestamp: "3h ago",
         message: "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
     },
+    
 ]
 </script>
   
@@ -52,22 +48,18 @@ body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
-
 .app {
     text-align: center;
 }
-
 .app-logo {
     height: 40vmin;
     pointer-events: none;
 }
-
 @media (prefers-reduced-motion: no-preference) {
     .app-logo {
         animation: App-logo-spin infinite 20s linear;
     }
 }
-
 .app-header {
     background-color: #282c34;
     min-height: 100vh;
@@ -78,25 +70,20 @@ body {
     font-size: calc(10px + 2vmin);
     color: white;
 }
-
 .app-link {
     color: #61dafb;
 }
-
 @keyframes App-logo-spin {
     from {
         transform: rotate(0deg);
     }
-
     to {
         transform: rotate(360deg);
     }
 }
-
 .app {
     padding: 20px;
 }
-
 .tweet {
     margin: 0 auto;
     min-width: 480px;
@@ -109,53 +96,44 @@ body {
     line-height: 18px;
     margin-bottom: 10px;
 }
-
 .body {
     display: flex;
     flex-direction: column;
     width: 100%;
 }
-
 .top {
     display: flex;
     line-height: 1.4rem;
 }
-
 .more {
     margin-left: 20px;
 }
-
 .profile {
     width: 50px;
     height: 50px;
     border-radius: 50px;
     margin-right: 10px;
 }
-
 .name {
     font-weight: bold;
     margin-bottom: 0.5em;
     margin-right: 0.3em;
 }
-
 .handle {
     color: #536471;
     margin-left: 3px;
     margin-right: 3px;
 }
-
 .timestamp {
     color: #536471;
     font-size: 1.1rem;
 }
-
 .timestamp::before {
     content: "·";
     padding-right: 0.3em;
     font-size: 1.1rem;
     font-weight: 600;
 }
-
 .message {
     text-align: left;
     font-stretch: 100%;
@@ -164,7 +142,6 @@ body {
     overflow-wrap: break-word;
     white-space: pre-wrap;
 }
-
 .actions {
     padding: 5px 5px;
     width: 360px;
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,15 +1,15 @@
-<template>
+<!-- <template>
 <Actions />
 <Message />
 <ProfileImage />
 <Timestamp />
 <User />
 
-<!-- <h3>{{otraInfoQuemMeLlegaDelPadre}}</h3> -->
+<h3>{{otraInfoQuemMeLlegaDelPadre}}</h3> 
 
     <div class="tweet">
         <img src="https://i.imgur.com/9yw1Fyw.jpg" class="profile" alt="profile" />
-
+ <ProfileImage/>
         <div class="body">
             <div class="top">
                 <span class="user">
@@ -31,7 +31,7 @@
             </p>
 
             <div class="actions">
-                <!-- Font Awesome icons -->
+                 Font Awesome icons 
                 <i class="far fa-comment"></i>
                 <i class="fas fa-retweet"></i>
                 <i class="far fa-heart"></i>
@@ -60,10 +60,46 @@ import {defineProps} from 'vue';
 const props = defineProps([
     'tweetHijo',
     
-    // 'otraInfoQuemMeLlegaDelPadre'
+     'otraInfoQuemMeLlegaDelPadre'
 ]
 
 )
 
 
+</script> -->
+<template>
+    <div class="tweet">
+        <ProfileImage :image="props.tweet.user.image" />
+        <div class="body">
+            <div class="top">
+                <User :name="props.tweet.user.name" :handle="props.tweet.user.handle" />
+                <!-- <User :userData="{name: props.tweet.user.name, handle: props.tweet.user.handle}" /> -->
+                <Timestamp :timestamp="props.tweet.timestamp" />
+            </div>
+            <Message :message="props.tweet.message" />
+            <Action />
+        </div>
+        <i class="fas fa-ellipsis-h"></i>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import ProfileImage from './ProfileImage.vue'
+import User from './User.vue';
+import Timestamp from './Timestamp.vue';
+import Message from './Message.vue';
+import Action from './Actions.vue'
+
+const props = defineProps({
+    tweet: Object
+})
+// No da informacion del tipo de prop que recibes
+// const props = defineProps(['tweet'])
 </script>
+  
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
